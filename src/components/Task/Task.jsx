0e1966ee3257937f1ab2b14fa292456a933ae281
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2025-09-09 10:03:44 +0900

    Taskコンポーネントでタスクを増減できるよう更新

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import './Task.css';
 
-function Task({
-  name,
-  setName,
-  tasktime,
-  setTasktime,
-  importance,
-  setImportance,
-}) {
-  const handleTaskInput = (e) => {
-    e.preventDefault();
-  };
-    
+function Task() {
+    const [tasks, setTasks] = useState([
+        { name: '', tasktime: '', importance: '1' }
+    ]);
+
+    const handleTaskChange = (index, field, value) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[index][field] = value;
+        setTasks(updatedTasks);
+    };
+
+    const handleAddTask = (e) => {
+        e.preventDefault();
+        setTasks([...tasks, { name: '', tasktime: '', importance: '1' }]);
+    };
+
+    const handleRemoveTask = (index) => {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    };
+
     return (
         <div>
             <form>
@@ -22,50 +31,62 @@ function Task({
                             <th>タスク名</th>
                             <th>所要時間</th>
                             <th>重要度</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        {tasks.map((task, index) => (
+                        <tr key={index}>
                             <td className='task-td-widthAuto'>
                                 <input
                                     type="text"
-                                    id='task-name'
-                                    value={name}
-                                    onChange={e => setName(e.target.value)}
+                                    value={task.name}
+                                    onChange={e => handleTaskChange(index, 'name', e.target.value)}
                                     placeholder='タスク名'
                                 />
                             </td>
                             <td className='task-td-width10'>
                                 <input
                                     type="number"
-                                    id='task-time'
-                                    value={tasktime}
-                                    onChange={e => setTasktime(e.target.value)}
+                                    min={1}
+                                    max={1440}
+                                    value={task.tasktime}
+                                    onChange={e => handleTaskChange(index, 'tasktime', e.target.value)}
                                 />
                             </td>
                             <td className='task-td-width10'>
-                                <select id="importance" value={importance} onChange={e => setImportance(e.target.value)}>
+                                <select
+                                    value={task.importance}
+                                    onChange={e => handleTaskChange(index, 'importance', e.target.value)}
+                                >
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
                                 </select>
                             </td>
                             <td className='task-td-width0'>
-                                <button className='close-button'>
+                                <button
+                                    type="button"
+                                    className='close-button'
+                                    onClick={() => handleRemoveTask(index)}
+                                    disabled={tasks.length === 1}
+                                >
                                     <span className="material-symbols-outlined">close</span>
                                 </button>
                             </td>
                         </tr>
+                        ))}
                     </tbody>
                 </table>
 
                 <div className='button-container'>
-                    <button type="submit" className='sub-button' onClick={handleTaskInput}>追加</button>
+                    <button type="submit" className='sub-button' onClick={handleAddTask}>
+                        追加
+                    </button>
                 </div>
             </form>
         </div>
-    )
+    );
 }
-
 
 export default Task;
