079e79545680fc71157b72f4992f7d302425c8ba
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2025-09-09 12:33:49 +0900

    ページ構造を修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SideMenu from './components/SideMenu/SideMenu'
 import PresetRegisterPage from './pages/PresetRegisterPage';
 import PresetListPage from './pages/PresetListPage';
+import PresetRunPage from './pages/PresetRunPage';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <main>
-        <SideMenu />
-        <section className='content-area'>
-          <div className='content'>
-            <Routes>
-              <Route path="/" element={<PresetListPage />} />
-              <Route path="/preset-list" element={<PresetListPage />} />
-              <Route path="/preset-register" element={<PresetRegisterPage />} />
-            </Routes>
-          </div>
-        </section>
-      </main>
+      <Routes>
+        <Route path="/" element={<PresetListPage />} />
+        <Route path="/preset-list" element={<PresetListPage />} />
+        <Route path="/preset-register" element={<PresetRegisterPage />} />
+        <Route path="/preset-run" element={<PresetRunPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
