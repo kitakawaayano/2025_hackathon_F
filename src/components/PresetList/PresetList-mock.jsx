bba97a7f8e5db68e974c309e11f4e947e5bb2da9
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-09-09 11:39:00 +0900

    プリセット一覧

--- src/components/PresetList/PresetList-mock.jsx
+++ src/components/PresetList/PresetList-mock.jsx
@@ -1,39 +1,118 @@
 import './PresetList.css';
+import React, { useState, useEffect } from 'react';
+
+const getPreset = async () => { 
+    const response = await fetch('http://localhost:3000/presets', {
+        method: 'GET',
+        header: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+const getTask = async () => { 
+    const response = await fetch('http://localhost:3000/tasks', {
+        method: 'GET',
+        header: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+
 
 function PresetList() {
+    const [data, setData] = useState([]);
+    const [tasks, setTasks] = useState([]);
+
+
+    useEffect(() => {
+        getPreset().then(result => {
+            setData(result);
+            // console.log(result);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (data.length > 0) {
+            data.map(count => {
+                getTask(count).then(result => {
+                    setTasks(result);
+                    // console.log(result);
+                });
+            });
+        }
+    }, [data]);
+
+    // console.log(tasks);
+
+    function taskCount(id){
+        let cnt = 0;
+        tasks.map(task =>{
+            if (id == task.preset_id){
+                cnt++;
+            }
+        })
+        return cnt;
+    }
+
+    function taskMinute(id){
+        let minute = 0;
+        tasks.map(task =>{
+            if (id == task.preset_id){
+                minute+=task.task_time;
+
+            }
+        })
+        console.log(minute)
+        return minute;
+    }
+
     return (
         <>
-            <div className='preset-list-container'>
-                <div className='preset-list-item'>
+        <div className='preset-list-container'>
+            {data.map(preset => 
+                <div className='preset-list-item' key={preset.id}>
                     <h3 className='preset-name'>
-                        プリセット名
+                        <li key={preset.id}>{preset.preset_name}</li>
                     </h3>
                     <ul className='preset-info-ul'>
                         <li>
+
                             <span className='preset-info-item'>
                                 <span className="material-symbols-outlined">alarm</span>
-                                <span>7:30</span>
+                                    <span key={preset.id}>{preset.finish_time}</span>
                             </span>
                         </li>
                         <li>
                             <span className='preset-info-item'>
                                 <span className="material-symbols-outlined">check_box</span>
-                                <span>5</span>
+                                <span>{taskCount(preset.id)}</span>
                                 <span className='preset-info-smallText'>個</span>
                             </span>
                         </li>
                         <li>
                             <span className="preset-info-item">
                                 <span className="material-symbols-outlined">timelapse</span>
-                                <span>30</span>
+                                <span>{taskMinute(preset.id)}</span>
                                 <span className='preset-info-smallText'>分</span>
                             </span>
                         </li>
                     </ul>
                 </div>
+                )}
             </div>
         </>
     );
 }
+
 
 export default PresetList;
