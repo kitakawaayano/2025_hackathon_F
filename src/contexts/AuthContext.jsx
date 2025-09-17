66e859e5390d707142bc929498bc3a0d4f54f5de
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-09-17 14:44:30 +0900

    urlを変更

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (username, password) => {
     try {
-      const response = await fetch("http://localhost:3000/users", {
+      const response = await fetch("https://2025-hackathon-f-json.vercel.app/users", {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
--- src/hooks/task.js
+++ src/hooks/task.js
@@ -1,6 +1,6 @@
 const postTask = async (tasks, presetid) => {
 
-    const response = await fetch('http://localhost:3000/tasks', {
+    const response = await fetch('https://2025-hackathon-f-json.vercel.app/tasks', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
