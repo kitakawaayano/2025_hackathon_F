f5e74b3bb1efae85efeb3912e3436ef169b74d74
kitakawaayano <a.kitakawa.sys24@morijyobi.ac.jp>
2025-09-18 10:48:53 +0900

    タイマーの修正

--- src/components/SideMenuRun/SideMenuRun.jsx
+++ src/components/SideMenuRun/SideMenuRun.jsx
@@ -153,6 +153,13 @@ function SideMenuRun({ filteredTasks, completedCount }) {
     return ("0" + num).slice(-2);
   };
 
+  useEffect(() => {
+    if (completedCount == taskCount && taskCount > 0){
+      setflg(false);
+    }
+  }, [completedCount])
+
+
   const location = useLocation();
 
   const navigate = useNavigate();
